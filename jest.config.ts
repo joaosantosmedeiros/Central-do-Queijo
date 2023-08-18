39bc5f113ce104ceb93d8a043a5e85d68868172b
joaosantosmedeiros <jopesame@gmail.com>
2023-08-18 11:08:28 -0300

    fix: change .drawio file and make jest ignore docker-data

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
+  modulePathIgnorePatterns: ['./docker-data/*'],
 };
 
 export default config;
