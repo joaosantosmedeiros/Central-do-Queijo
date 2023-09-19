4c6a0b39b847aa808c0a6457f63f28cf019d0072
joaosantosmedeiros <jopesame@gmail.com>
2023-09-19 19:39:41 -0300

    test: ensure CreateCategoryUseCase creates an account correctly

---
+++ src/application/usecases/category-usecases/create-category-usecase.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
+import { CreateCategoryUseCase } from './create-category-usecase';
+import { CategoryAlreadyExistsError } from '../errors/category-already-exists-error';
+
+describe('Create Category Use Case', () => {
+  it('should be able to create a new category', async () => {
+    const categoryRepository = new InMemoryCategoryRepository();
+    const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+
+    const { category } = await createCategoryUseCase.execute({
+      name: 'any_name',
+    });
+
+    expect(category).toBeTruthy();
+  });
+
+  it('should not be able to create an category with an used name', async () => {
+    const categoryRepository = new InMemoryCategoryRepository();
+    const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+
+    await createCategoryUseCase.execute({
+      name: 'any_name',
+    });
+
+    expect(async () => {
+      await createCategoryUseCase.execute({
+        name: 'any_name',
+      });
+    }).rejects.toThrow(CategoryAlreadyExistsError);
+  });
+});
