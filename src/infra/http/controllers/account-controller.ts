f52ca2b8af54e91fc450d077391fa9cc20a3673c
joaosantosmedeiros <jopesame@gmail.com>
2023-09-20 19:03:57 -0300

    fix: fix imports from account usecases

--- src/infra/http/controllers/account-controller.ts
+++ src/infra/http/controllers/account-controller.ts
@@ -1,13 +1,13 @@
-import { CreateAccountUseCase } from '@application/usecases/create-account-usecase';
+import { CreateAccountUseCase } from '@application/usecases/account-usecases/create-account-usecase';
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateAccountBody } from '../dto/create-account-body';
 import { PasswordDontMatchException } from '../exceptions/password-dont-match-exception';
 import { EmailInUseException } from '../exceptions/email-in-use-exception';
-import { ListAllAccountsUseCase } from '@application/usecases/list-accounts-usecase';
-import { FindAccountByEmailUseCase } from '@application/usecases/find-account-by-email-usecase';
+import { ListAllAccountsUseCase } from '@application/usecases/account-usecases/list-accounts-usecase';
+import { FindAccountByEmailUseCase } from '@application/usecases/account-usecases/find-account-by-email-usecase';
 import { Account } from '@application/entities/account/account';
 import { AccountNotFoundException } from '../exceptions/account-not-found-exception';
-import { DeleteAccountUseCase } from '@application/usecases/delete-account-usecase';
+import { DeleteAccountUseCase } from '@application/usecases/account-usecases/delete-account-usecase';
 
 @Controller('account')
 export class AccountController {
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './controllers/account-controller';
-import { CreateAccountUseCase } from '@application/usecases/create-account-usecase';
+import { CreateAccountUseCase } from '@application/usecases/account-usecases/create-account-usecase';
 import { DatabaseModule } from '@infra/database/database.module';
-import { ListAllAccountsUseCase } from '@application/usecases/list-accounts-usecase';
-import { FindAccountByEmailUseCase } from '@application/usecases/find-account-by-email-usecase';
-import { DeleteAccountUseCase } from '@application/usecases/delete-account-usecase';
+import { ListAllAccountsUseCase } from '@application/usecases/account-usecases/list-accounts-usecase';
+import { FindAccountByEmailUseCase } from '@application/usecases/account-usecases/find-account-by-email-usecase';
+import { DeleteAccountUseCase } from '@application/usecases/account-usecases/delete-account-usecase';
 
 @Module({
   imports: [DatabaseModule],
