8e49ff6fc83a9c0cde5d4a252b370ca844f13a45
joaosantosmedeiros <jopesame@gmail.com>
2023-09-20 19:27:07 -0300

    test: ensure DeleteCategoryUseCase is working correctly

---
+++ src/application/usecases/category-usecases/delete-category-usecase.spec.ts
@@ -0,0 +1,29 @@
+import { makeCategory } from '@test/factories/category-factory';
+import { DeleteCategoryUseCase } from './delete-category-usecase';
+import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
+
+describe('DeleteCategoryUseCase', () => {
+  it('should delete an account correctly', async () => {
+    const inMemoryCategoryRepository = new InMemoryCategoryRepository();
+    const deleteCategoryUseCase = new DeleteCategoryUseCase(
+      inMemoryCategoryRepository,
+    );
+
+    inMemoryCategoryRepository.create(makeCategory());
+    const category = inMemoryCategoryRepository.categories[0];
+
+    await deleteCategoryUseCase.execute(category.id);
+    expect(inMemoryCategoryRepository.categories.length).toBe(0);
+  });
+
+  it('should not delete an unexistent account', async () => {
+    const inMemoryCategoryRepository = new InMemoryCategoryRepository();
+    const deleteCategoryUseCase = new DeleteCategoryUseCase(
+      inMemoryCategoryRepository,
+    );
+
+    expect(
+      async () => await deleteCategoryUseCase.execute('fake_id'),
+    ).rejects.toThrow(Error);
+  });
+});
