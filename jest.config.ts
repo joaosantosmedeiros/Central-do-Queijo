c11d0c9056f60f0deaaeecc9165aa58fe63d2de6
joaosantosmedeiros <jopesame@gmail.com>
2023-09-20 19:26:50 -0300

    feat: ensure DeleteCategoryUseCase is working correctly

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: [
-    '**/*.(t|j)s',
+    '**/*.ts',
     '!**/*.module.*',
     '!**/*.controller.*',
     '!**/*.service.*',
