3780baa23e6525e0bdb1d10799d49867525523b0
joaosantosmedeiros <jopesame@gmail.com>
2023-09-20 19:21:40 -0300

    fix: add categories attribute to CategoryRepository

--- src/application/repositories/category-repository.ts
+++ src/application/repositories/category-repository.ts
@@ -1,6 +1,8 @@
 import { Category } from '@application/entities/category/category';
 
 export abstract class CategoryRepository {
+  public categories: Category[];
+
   abstract findById(id: string): Promise<Category | null>;
   abstract list(): Promise<Category[]>;
   abstract create(category: Category): Promise<void>;
