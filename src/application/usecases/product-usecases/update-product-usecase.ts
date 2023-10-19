113065ef8bfdd4c9dadab04822af4d9d05a15a59
joaosantosmedeiros <jopesame@gmail.com>
2023-10-19 20:56:40 -0300

    feat: add UpdateProductUseCase

---
+++ src/application/usecases/product-usecases/update-product-usecase.ts
@@ -0,0 +1,20 @@
+import { ProductRepository } from '@application/repositories/product-repository';
+import { Injectable } from '@nestjs/common';
+
+export interface UpdateProductRequest {
+  id: string;
+  name: string;
+  categoryId: string;
+}
+
+@Injectable()
+export class UpdateProductUseCase {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(request: UpdateProductRequest) {
+    return this.productRepository.update(request.id, {
+      name: request.name,
+      categoryId: request.categoryId,
+    });
+  }
+}
