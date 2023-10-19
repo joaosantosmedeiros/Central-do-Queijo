04817e8964b9f88d090c5139bef1363ae19d6124
joaosantosmedeiros <jopesame@gmail.com>
2023-10-18 22:27:03 -0300

    test: ensure ListProductsUseCase works properly

---
+++ src/application/usecases/product-usecases/list-products-usecase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryProductsRepository } from '@test/repositories/in-memory-products-repository';
+import { ListProductsUseCase } from './list-products-usecase';
+import { makeProduct } from '@test/factories/product-factory';
+
+describe('List Products Use Case', () => {
+  it('should list products correctly', async () => {
+    const inMemoryProductRepository = new InMemoryProductsRepository();
+    const listProductsUseCase = new ListProductsUseCase(
+      inMemoryProductRepository,
+    );
+    const expected = 2;
+
+    inMemoryProductRepository.categoriesIds = ['any_id'];
+
+    inMemoryProductRepository.create(makeProduct());
+    inMemoryProductRepository.create(makeProduct());
+
+    const actual = (await listProductsUseCase.execute()).length;
+
+    expect(actual).toBe(expected);
+  });
+});
