b25a25531e7fa790dfcdcb9d32b7f4c4b4c1c9a6
joaosantosmedeiros <jopesame@gmail.com>
2023-10-23 14:27:08 -0300

    test: ensure UpdateProductUseCase works properly

---
+++ src/application/usecases/product-usecases/update-product-usecase.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryProductsRepository } from '@test/repositories/in-memory-products-repository';
+import { UpdateProductUseCase } from './update-product-usecase';
+import { makeProduct } from '@test/factories/product-factory';
+
+describe('Update Product UseCase', () => {
+  it('should update a product properly', async () => {
+    const inMemoryProductRepository = new InMemoryProductsRepository();
+    const updateProductUseCase = new UpdateProductUseCase(
+      inMemoryProductRepository,
+    );
+    const expected = 'updated_name';
+
+    inMemoryProductRepository.categoriesIds = ['any_id', 'updated_id'];
+    await inMemoryProductRepository.create(makeProduct());
+    const product = inMemoryProductRepository.products[0];
+
+    const updatedProduct = await updateProductUseCase.execute({
+      id: product.id,
+      name: 'updated_name',
+      categoryId: 'updated_id',
+    });
+    const actual = updatedProduct.name;
+
+    expect(actual).toBe(expected);
+    expect(updatedProduct.updatedAt == updatedProduct.createdAt).toBeFalsy();
+  });
+
+  it('should not update an unexistent product', async () => {
+    const inMemoryProductRepository = new InMemoryProductsRepository();
+    const updateProductUseCase = new UpdateProductUseCase(
+      inMemoryProductRepository,
+    );
+
+    expect(
+      async () =>
+        await updateProductUseCase.execute({
+          id: 'fake_id',
+          name: 'any_name',
+          categoryId: 'updated_id',
+        }),
+    ).rejects.toThrow();
+  });
+});
