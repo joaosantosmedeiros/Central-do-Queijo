f045d1214b2c53dcc7d41d52242d9bbffc24b67b
joaosantosmedeiros <jopesame@gmail.com>
2023-11-20 13:27:11 -0300

    test: ensure Payment class works properly

---
+++ src/application/entities/payment/payment.spec.ts
@@ -0,0 +1,41 @@
+import { PaymentStatus } from '../../../enums/payment-status.enum';
+import { Payment } from './payment';
+
+describe('Payment', () => {
+  it('should create a new payment with correct values', () => {
+    const payment = new Payment({ code: 'any_code', discount: 20, price: 100 });
+
+    expect(payment.id).toBeTruthy();
+    expect(payment.status).toBe(PaymentStatus.PENDING);
+    expect(payment.price).toBe(100);
+    expect(payment.discount).toBe(20);
+    expect(payment.finalPrice).toBe(80);
+    expect(payment.code).toBe('any_code');
+    expect(payment.paymentDate).toBeUndefined();
+    expect(payment.updatedAt).toEqual(payment.createdAt);
+  });
+
+  it('should update the finalPrice if the discount is altered', () => {
+    const payment = new Payment({ code: 'any_code', discount: 20, price: 100 });
+
+    payment.discount = 10;
+    expect(payment.finalPrice).toBe(90);
+  });
+
+  it('should update the finalPrice if the price is altered', () => {
+    const payment = new Payment({ code: 'any_code', discount: 20, price: 100 });
+
+    payment.price = 200;
+    expect(payment.finalPrice).toBe(160);
+  });
+
+  it('should not be able to pay twice', () => {
+    const payment = new Payment({ code: 'any_code', discount: 20, price: 100 });
+
+    payment.pay();
+
+    async () => {
+      expect(payment.pay()).rejects.toThrow();
+    };
+  });
+});
