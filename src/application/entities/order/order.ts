37d6c38bb0a3c4602bfeb683154370a58f7d097a
joaosantosmedeiros <jopesame@gmail.com>
2023-11-21 20:04:06 -0300

    feat: add Order model

---
+++ src/application/entities/order/order.ts
@@ -0,0 +1,55 @@
+import { Replace } from '@helpers/replace';
+import { randomUUID } from 'crypto';
+
+export interface OrderProps {
+  accountId: string;
+  date: Date;
+  paymentId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Order {
+  private _id: string;
+  private props: OrderProps;
+
+  constructor(
+    props: Replace<
+      OrderProps,
+      { date?: Date; createdAt?: Date; updatedAt?: Date }
+    >,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+      date: new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public get accountId() {
+    return this.props.accountId;
+  }
+
+  public get date() {
+    return this.props.date;
+  }
+
+  public get paymentId() {
+    return this.props.accountId;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
