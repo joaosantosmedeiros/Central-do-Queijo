7ff6fe02b138e564a0afaa9171b26fa5eb3b6e63
joaosantosmedeiros <jopesame@gmail.com>
2023-11-21 20:12:27 -0300

    feat: add OrderProduct entity

---
+++ src/application/entities/order-product/order-product.ts
@@ -0,0 +1,72 @@
+import { Replace } from '@helpers/replace';
+import { randomUUID } from 'crypto';
+import { Order } from '../order/order';
+import { Product } from '../product/product';
+
+export interface OrderProductProps {
+  orderId: string;
+  order?: Order;
+  productId: string;
+  product?: Product;
+  amount: number;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class OrderProduct {
+  private _id: string;
+  private props: OrderProductProps;
+
+  constructor(
+    props: Replace<OrderProductProps, { createdAt?: Date; updatedAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public get orderId() {
+    return this.props.orderId;
+  }
+
+  public get productId() {
+    return this.props.productId;
+  }
+
+  public get amount() {
+    return this.props.amount;
+  }
+  public set amount(amount: number) {
+    this.props.amount = amount;
+    this.props.updatedAt = new Date();
+  }
+
+  public get price() {
+    return this.props.price;
+  }
+  public set price(price: number) {
+    this.props.price = price;
+    this.props.updatedAt = new Date();
+  }
+
+  public get product() {
+    return this.props.product;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
