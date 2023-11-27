cf5e29513a178d8a9e61aa55aa10e92454692cc9
joaosantosmedeiros <jopesame@gmail.com>
2023-11-27 19:01:18 -0300

    feat: add search with ids array in ListProductsUseCase

--- src/application/repositories/product-repository.ts
+++ src/application/repositories/product-repository.ts
@@ -3,7 +3,7 @@ import { Product } from '@application/entities/product/product';
 export abstract class ProductRepository {
   abstract findById(id: string): Promise<Product | null>;
   abstract findByName(name: string): Promise<Product | null>;
-  abstract list(): Promise<Product[]>;
+  abstract list(productsId?: string[]): Promise<Product[]>;
   abstract create(product: Product): Promise<void>;
   abstract update(
     id: string,
