9d79132fa1d9bbf4830346ca34c38f6c9ba915e8
joaosantosmedeiros <jopesame@gmail.com>
2023-11-27 14:31:20 -0300

    fix: adjust product factory

--- test/factories/product-factory.ts
+++ test/factories/product-factory.ts
@@ -1,16 +1,16 @@
 import { Product } from '@application/entities/product/product';
-import { randomUUID } from 'crypto';
 
 export function makeProduct(
   name = 'any_name',
-  categoryId = 'any_id',
+  price = 1,
+  id = 'any_id',
+  categoryId = 'any_category_id',
   image = 'any_image',
   createdAt = new Date(),
   updatedAt = new Date(),
-  id = randomUUID(),
 ) {
   return new Product(
-    { name, categoryId, createdAt, updatedAt, price: 1, image },
+    { name, categoryId, createdAt, updatedAt, price, image },
     id,
   );
 }
