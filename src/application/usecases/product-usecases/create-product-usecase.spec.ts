0c9455e09ed72d4e25dc8fed019786ce2e0805be
joaosantosmedeiros <jopesame@gmail.com>
2023-11-28 10:28:33 -0300

    fix: fix CreateProduct to throw if an invalid category is provided

--- src/application/usecases/product-usecases/create-product-usecase.spec.ts
+++ src/application/usecases/product-usecases/create-product-usecase.spec.ts
@@ -1,16 +1,30 @@
 import { InMemoryProductsRepository } from '@test/repositories/in-memory-products-repository';
 import { CreateProductUseCase } from './create-product-usecase';
+import { FindCategoryByIdUseCase } from '../category-usecases';
+import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
+import { Category } from '@application/entities/category/category';
+
+const makeSut = () => {
+  const categoryRepository = new InMemoryCategoryRepository();
+  const createProduct = new CreateProductUseCase(
+    new InMemoryProductsRepository(),
+    new FindCategoryByIdUseCase(categoryRepository),
+  );
+
+  return { categoryRepository, createProduct };
+};
 
 describe('CreateProduct Use Case', () => {
   it('should be able to create a new product', async () => {
-    const productRepository = new InMemoryProductsRepository();
-    const createProductUseCase = new CreateProductUseCase(productRepository);
+    const { categoryRepository, createProduct } = makeSut();
 
-    productRepository.categoriesIds.push('any_id');
+    categoryRepository.categories.push(
+      new Category({ name: 'any_category' }, 'any_category_id'),
+    );
 
-    const product = await createProductUseCase.execute({
+    const product = await createProduct.execute({
       name: 'any_name',
-      categoryId: 'any_id',
+      categoryId: 'any_category_id',
       price: 1,
       image: 'any_image',
     });
@@ -19,11 +33,10 @@ describe('CreateProduct Use Case', () => {
   });
 
   it('should not be able to create a product with an invalid category id', async () => {
-    const productRepository = new InMemoryProductsRepository();
-    const createProductUseCase = new CreateProductUseCase(productRepository);
+    const { createProduct } = makeSut();
 
     expect(async () => {
-      await createProductUseCase.execute({
+      await createProduct.execute({
         name: 'any_name',
         categoryId: 'invalid_id',
         price: 1,
--- src/application/usecases/product-usecases/create-product-usecase.ts
+++ src/application/usecases/product-usecases/create-product-usecase.ts
@@ -1,6 +1,7 @@
 import { Product } from '@application/entities/product/product';
 import { ProductRepository } from '@application/repositories/product-repository';
 import { Injectable } from '@nestjs/common';
+import { FindCategoryByIdUseCase } from '../category-usecases';
 
 export interface CreateProductRequest {
   name: string;
@@ -11,10 +12,15 @@ export interface CreateProductRequest {
 
 @Injectable()
 export class CreateProductUseCase {
-  constructor(private productRepository: ProductRepository) {}
+  constructor(
+    private productRepository: ProductRepository,
+    private findCategoryById: FindCategoryByIdUseCase,
+  ) {}
 
   async execute(request: CreateProductRequest): Promise<Product> {
     const { name, categoryId, price, image } = request;
+
+    await this.findCategoryById.execute(categoryId);
 
     const product = new Product({
       name,
