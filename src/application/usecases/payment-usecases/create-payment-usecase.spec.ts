6e751d1db592fd0982bdbab7ca3d3c21bdc0bca9
joaosantosmedeiros <jopesame@gmail.com>
2023-11-28 08:46:03 -0300

    feat: update CreatePaymentUseCase

--- src/application/usecases/payment-usecases/create-payment-usecase.spec.ts
+++ src/application/usecases/payment-usecases/create-payment-usecase.spec.ts
@@ -1,5 +1,9 @@
 import { InMemoryPaymentsRepository } from '@test/repositories/in-memory-payments-repository';
 import { CreatePaymentUseCase } from './create-payment-usecase';
+import { makeProduct } from '@test/factories/product-factory';
+import { Product } from '@application/entities/product/product';
+import { Cart } from '@application/entities/cart/cart';
+import { CartProduct } from '@application/entities/cart-product/cart-product';
 
 describe('CreatePaymentUsecase', () => {
   it('should create a payment with correct values', async () => {
@@ -10,16 +14,142 @@ describe('CreatePaymentUsecase', () => {
 
     const code = 'any_code';
     const discount = 0;
-    const price = 50;
 
-    const payment = await createPaymentUseCase.execute({
-      code,
-      discount,
-      price,
-    });
+    const products: Product[] = [
+      makeProduct('p1', 1, '1'),
+      makeProduct('p2', 2, '2'),
+    ];
 
-    expect(payment.code).toBe(code);
+    const cartProducts: CartProduct[] = [
+      new CartProduct({ cartId: 'cart_id', amount: 5, productId: '1' }),
+      new CartProduct({ cartId: 'cart_id', amount: 3, productId: '2' }),
+    ];
+
+    const price = 5 * 1 + 3 * 2;
+
+    const cart = new Cart(
+      {
+        accountId: 'any_acc_id',
+        cartProduct: cartProducts,
+      },
+      'cart_id',
+    );
+
+    const payment = await createPaymentUseCase.execute(
+      {
+        code,
+        discount,
+      },
+      products,
+      cart,
+    );
+
     expect(payment.code).toBe(code);
     expect(payment.price).toBe(price);
+  });
+
+  it('should create a payment with 0 as final price if no CartProduct is found.', async () => {
+    const inMemoryPaymentsRepository = new InMemoryPaymentsRepository();
+    const createPaymentUseCase = new CreatePaymentUseCase(
+      inMemoryPaymentsRepository,
+    );
+
+    const code = 'any_code';
+    const discount = 0;
+
+    const products: Product[] = [
+      makeProduct('p1', 1, '1'),
+      makeProduct('p2', 2, '2'),
+    ];
+
+    const cart = new Cart(
+      {
+        accountId: 'any_acc_id',
+        cartProduct: [],
+      },
+      'cart_id',
+    );
+
+    const payment = await createPaymentUseCase.execute(
+      { code, discount },
+      products,
+      cart,
+    );
+
+    expect(payment.code).toBe(code);
+    expect(payment.price).toBe(0);
+  });
+
+  it('should create a payment with 0 as final price if CartProduct products are not found.', async () => {
+    const inMemoryPaymentsRepository = new InMemoryPaymentsRepository();
+    const createPaymentUseCase = new CreatePaymentUseCase(
+      inMemoryPaymentsRepository,
+    );
+
+    const code = 'any_code';
+    const discount = 0;
+
+    const products: Product[] = [
+      makeProduct('p1', 1, '1'),
+      makeProduct('p2', 2, '2'),
+    ];
+
+    const cartProducts: CartProduct[] = [
+      new CartProduct({
+        cartId: 'cart_id',
+        amount: 5,
+        productId: '1',
+      }),
+      new CartProduct({
+        cartId: 'cart_id',
+        amount: 3,
+        productId: 'invalid_id',
+      }),
+    ];
+
+    const cart = new Cart(
+      {
+        accountId: 'any_acc_id',
+        cartProduct: cartProducts,
+      },
+      'cart_id',
+    );
+
+    const payment = await createPaymentUseCase.execute(
+      { code, discount },
+      products,
+      cart,
+    );
+
+    expect(payment.code).toBe(code);
+    expect(payment.price).toBe(5);
+  });
+
+  it('should throw if no CartProducts exist.', async () => {
+    const inMemoryPaymentsRepository = new InMemoryPaymentsRepository();
+    const createPaymentUseCase = new CreatePaymentUseCase(
+      inMemoryPaymentsRepository,
+    );
+
+    const code = 'any_code';
+    const discount = 0;
+
+    const products: Product[] = [
+      makeProduct('p1', 1, '1'),
+      makeProduct('p2', 2, '2'),
+    ];
+
+    const cart = new Cart(
+      {
+        accountId: 'any_acc_id',
+      },
+      'cart_id',
+    );
+
+    async () => {
+      expect(
+        await createPaymentUseCase.execute({ code, discount }, products, cart),
+      ).rejects.toThrow();
+    };
   });
 });
--- src/application/usecases/payment-usecases/create-payment-usecase.ts
+++ src/application/usecases/payment-usecases/create-payment-usecase.ts
@@ -1,19 +1,46 @@
+import { Cart } from '@application/entities/cart/cart';
 import { Payment } from '@application/entities/payment/payment';
+import { Product } from '@application/entities/product/product';
 import { PaymentRepository } from '@application/repositories/payment-repository';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 export interface CreatePaymentRequest {
   code: string;
   discount: number;
-  price: number;
 }
 
 @Injectable()
 export class CreatePaymentUseCase {
   constructor(private paymentRepository: PaymentRepository) {}
 
-  async execute(request: CreatePaymentRequest): Promise<Payment> {
-    const { code, discount, price } = request;
+  generatePrice(cart: Cart, products: Product[]): number {
+    if (!cart.cartProduct || cart.cartProduct.length == 0) {
+      return 0;
+    }
+
+    return cart.cartProduct.reduce((acc, cartProduct) => {
+      const product = products.find(
+        (prodct) => prodct.id === cartProduct.productId,
+      );
+      if (product) {
+        return acc + product?.price * cartProduct.amount;
+      }
+      return acc;
+    }, 0);
+  }
+
+  async execute(
+    request: CreatePaymentRequest,
+    products: Product[],
+    cart: Cart,
+  ): Promise<Payment> {
+    if (!cart.cartProduct) {
+      throw new HttpException('The cart is empty.', HttpStatus.BAD_REQUEST);
+    }
+
+    const price = this.generatePrice(cart, products);
+
+    const { code, discount } = request;
 
     const payment = new Payment({ code, discount, price });
 
