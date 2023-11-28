53ad0574a3a36f25730964eec864117976890aab
joaosantosmedeiros <jopesame@gmail.com>
2023-11-28 10:23:28 -0300

    fix: fix FindCategoryById to throw if no category is found

--- src/application/usecases/category-usecases/find-category-by-id-usecase.spec.ts
+++ src/application/usecases/category-usecases/find-category-by-id-usecase.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
 import { FindCategoryByIdUseCase } from './find-category-by-id-usecase';
 import { makeCategory } from '@test/factories/category-factory';
+import { EntityNotFoundException } from '@infra/http/exceptions/entity-not-found-exception';
 
 describe('Find Category By Id', () => {
   it('should find an existing category', async () => {
@@ -18,14 +19,16 @@ describe('Find Category By Id', () => {
     expect(category).toBe(categories[1]);
   });
 
-  it('should return null if no category is found', async () => {
+  it('should throw if no category is found', async () => {
     const inMemoryCategoryRepository = new InMemoryCategoryRepository();
     const findCategoryByIdUseCase = new FindCategoryByIdUseCase(
       inMemoryCategoryRepository,
     );
 
-    const category = await findCategoryByIdUseCase.execute('fake_id');
-
-    expect(category).toBeNull();
+    async () => {
+      expect(await findCategoryByIdUseCase.execute('fake_id')).rejects.toThrow(
+        EntityNotFoundException,
+      );
+    };
   });
 });
--- src/application/usecases/category-usecases/find-category-by-id-usecase.ts
+++ src/application/usecases/category-usecases/find-category-by-id-usecase.ts
@@ -1,12 +1,17 @@
 import { Category } from '@application/entities/category/category';
 import { CategoryRepository } from '@application/repositories/category-repository';
+import { EntityNotFoundException } from '@infra/http/exceptions/entity-not-found-exception';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FindCategoryByIdUseCase {
   constructor(private readonly categoryRepository: CategoryRepository) {}
 
-  async execute(id: string): Promise<Category | null> {
-    return this.categoryRepository.findById(id);
+  async execute(id: string): Promise<Category> {
+    const category = await this.categoryRepository.findById(id);
+    if (!category) {
+      throw new EntityNotFoundException('Category');
+    }
+    return category;
   }
 }
