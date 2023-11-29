0301a2120b78c0aa79046b745515d7412877f09b
joaosantosmedeiros <jopesame@gmail.com>
2023-11-29 08:33:55 -0300

    fix: fix FindProductByName to throw if no product is found

--- src/application/usecases/product-usecases/find-product-by-name-usecase.spec.ts
+++ src/application/usecases/product-usecases/find-product-by-name-usecase.spec.ts
@@ -1,32 +1,30 @@
 import { makeProduct } from '@test/factories/product-factory';
 import { InMemoryProductsRepository } from '@test/repositories/in-memory-products-repository';
 import { FindProductByNameUseCase } from './find-product-by-name-usecase';
+import { EntityNotFoundException } from '@infra/http/exceptions/entity-not-found-exception';
 
 describe('Find Product By Name', () => {
   it('should find an existing product', async () => {
-    const inMemoryProductRepository = new InMemoryProductsRepository();
-    const findProductByNameUseCase = new FindProductByNameUseCase(
-      inMemoryProductRepository,
-    );
+    const productRepository = new InMemoryProductsRepository();
+    const findProductByName = new FindProductByNameUseCase(productRepository);
 
-    inMemoryProductRepository.categoriesIds = ['any_category_id'];
-    await inMemoryProductRepository.create(makeProduct());
-    await inMemoryProductRepository.create(makeProduct('product_2'));
+    await productRepository.create(makeProduct());
+    await productRepository.create(makeProduct('product_2'));
 
-    const products = await inMemoryProductRepository.list();
-    const product = await findProductByNameUseCase.execute(products[1].name);
+    const products = await productRepository.list();
+    const product = await findProductByName.execute(products[1].name);
 
     expect(product).toBe(products[1]);
   });
 
-  it('should return null if no product is found', async () => {
-    const inMemoryProductRepository = new InMemoryProductsRepository();
-    const findProductByNameUseCase = new FindProductByNameUseCase(
-      inMemoryProductRepository,
-    );
+  it('should throw if no product is found', async () => {
+    const productRepository = new InMemoryProductsRepository();
+    const findProductByName = new FindProductByNameUseCase(productRepository);
 
-    const product = await findProductByNameUseCase.execute('fake_id');
-
-    expect(product).toBeNull();
+    async () => {
+      expect(await findProductByName.execute('fake_id')).rejects.toThrow(
+        EntityNotFoundException,
+      );
+    };
   });
 });
--- src/application/usecases/product-usecases/find-product-by-name-usecase.ts
+++ src/application/usecases/product-usecases/find-product-by-name-usecase.ts
@@ -1,12 +1,17 @@
 import { Product } from '@application/entities/product/product';
 import { ProductRepository } from '@application/repositories/product-repository';
+import { EntityNotFoundException } from '@infra/http/exceptions/entity-not-found-exception';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FindProductByNameUseCase {
   constructor(private readonly productRepository: ProductRepository) {}
 
-  async execute(id: string): Promise<Product | null> {
-    return this.productRepository.findByName(id);
+  async execute(id: string): Promise<Product> {
+    const product = await this.productRepository.findByName(id);
+    if (!product) {
+      throw new EntityNotFoundException('Product');
+    }
+    return product;
   }
 }
