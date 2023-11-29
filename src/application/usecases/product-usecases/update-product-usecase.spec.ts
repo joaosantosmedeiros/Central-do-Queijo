54b94458594ecd6a85b5ac8097ff66b19a5576b2
joaosantosmedeiros <jopesame@gmail.com>
2023-11-29 08:36:24 -0300

    fix: fix UpdateProduct to to throw if an invalid category is provided

--- src/application/usecases/product-usecases/update-product-usecase.spec.ts
+++ src/application/usecases/product-usecases/update-product-usecase.spec.ts
@@ -1,41 +1,53 @@
 import { InMemoryProductsRepository } from '@test/repositories/in-memory-products-repository';
 import { UpdateProductUseCase } from './update-product-usecase';
 import { makeProduct } from '@test/factories/product-factory';
+import { FindCategoryByIdUseCase } from '../category-usecases';
+import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
+import { Category } from '@application/entities/category/category';
+
+const makeSut = () => {
+  const categoryRepository = new InMemoryCategoryRepository();
+  const productRepository = new InMemoryProductsRepository();
+  const updateProduct = new UpdateProductUseCase(
+    productRepository,
+    new FindCategoryByIdUseCase(categoryRepository),
+  );
+
+  return { categoryRepository, updateProduct, productRepository };
+};
 
 describe('Update Product UseCase', () => {
   it('should update a product properly', async () => {
-    const inMemoryProductRepository = new InMemoryProductsRepository();
-    const updateProductUseCase = new UpdateProductUseCase(
-      inMemoryProductRepository,
+    const { categoryRepository, productRepository, updateProduct } = makeSut();
+
+    categoryRepository.categories.push(
+      new Category({ name: 'any_name' }, 'any_category_id'),
+      new Category({ name: 'another_name' }, 'updated_id'),
     );
-    const expected = 'updated_name';
 
-    inMemoryProductRepository.categoriesIds = ['any_category_id', 'updated_id'];
-    await inMemoryProductRepository.create(makeProduct());
-    const product = inMemoryProductRepository.products[0];
+    await productRepository.create(
+      makeProduct('any_product', 4, '1', 'any_category_id'),
+    );
+    const product = productRepository.products[0];
 
-    const updatedProduct = await updateProductUseCase.execute({
+    const updatedProduct = await updateProduct.execute({
       id: product.id,
       name: 'updated_name',
       categoryId: 'updated_id',
       price: 33,
       image: 'updated_image',
     });
-    const actual = updatedProduct.name;
 
-    expect(actual).toBe(expected);
+    expect(updatedProduct.name).toBe('updated_name');
     expect(updatedProduct.updatedAt == updatedProduct.createdAt).toBeFalsy();
   });
 
   it('should not update an unexistent product', async () => {
-    const inMemoryProductRepository = new InMemoryProductsRepository();
-    const updateProductUseCase = new UpdateProductUseCase(
-      inMemoryProductRepository,
-    );
+    const { updateProduct } = makeSut();
 
     expect(
       async () =>
-        await updateProductUseCase.execute({
+        await updateProduct.execute({
           id: 'fake_id',
           name: 'any_name',
           categoryId: 'updated_id',
@@ -46,19 +58,19 @@ describe('Update Product UseCase', () => {
   });
 
   it('should not update a product with invalid category', async () => {
-    const inMemoryProductRepository = new InMemoryProductsRepository();
-    const updateProductUseCase = new UpdateProductUseCase(
-      inMemoryProductRepository,
+    const { categoryRepository, productRepository, updateProduct } = makeSut();
+
+    categoryRepository.categories.push(
+      new Category({ name: 'any_name' }, 'any_category_id'),
     );
 
-    inMemoryProductRepository.categoriesIds = ['any_category_id'];
-    await inMemoryProductRepository.create(makeProduct());
+    await productRepository.create(makeProduct());
 
-    const product = inMemoryProductRepository.products[0];
+    const product = productRepository.products[0];
 
     expect(
       async () =>
-        await updateProductUseCase.execute({
+        await updateProduct.execute({
           id: product.id,
           name: 'any_name',
           categoryId: 'updated_id',
--- src/application/usecases/product-usecases/update-product-usecase.ts
+++ src/application/usecases/product-usecases/update-product-usecase.ts
@@ -1,6 +1,7 @@
 import { Product } from '@application/entities/product/product';
 import { ProductRepository } from '@application/repositories/product-repository';
 import { Injectable } from '@nestjs/common';
+import { FindCategoryByIdUseCase } from '../category-usecases';
 
 export interface UpdateProductRequest {
   id: string;
@@ -12,12 +13,17 @@ export interface UpdateProductRequest {
 
 @Injectable()
 export class UpdateProductUseCase {
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly findCategoryById: FindCategoryByIdUseCase,
+  ) {}
 
   async execute(request: UpdateProductRequest): Promise<Product> {
+    const category = await this.findCategoryById.execute(request.categoryId);
+
     return this.productRepository.update(request.id, {
       name: request.name,
-      categoryId: request.categoryId,
+      categoryId: category.id,
       image: request.image,
       price: request.price,
     });
