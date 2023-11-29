7557b4e8605abdf8eb35159950f1a4f20a809ae2
joaosantosmedeiros <jopesame@gmail.com>
2023-11-29 09:15:02 -0300

    fix: fix CreateCategory to throw if a category already uses the same name

--- src/application/usecases/category-usecases/create-category-usecase.spec.ts
+++ src/application/usecases/category-usecases/create-category-usecase.spec.ts
@@ -1,13 +1,22 @@
 import { InMemoryCategoryRepository } from '@test/repositories/in-memory-categories-repository';
 import { CreateCategoryUseCase } from './create-category-usecase';
-import { EntityAlreadyExistsError } from '../errors/entity-already-exists-error';
+import { FindCategoryByNameUseCase } from './find-category-by-name-usecase';
+import { CategoryAlreadyExistsException } from '@infra/http/exceptions/category-already-exists-exception';
+
+const makeSut = () => {
+  const categoryRepository = new InMemoryCategoryRepository();
+  const createCategory = new CreateCategoryUseCase(
+    categoryRepository,
+    new FindCategoryByNameUseCase(categoryRepository),
+  );
+  return { categoryRepository, createCategory };
+};
 
 describe('Create Category Use Case', () => {
   it('should be able to create a new category', async () => {
-    const categoryRepository = new InMemoryCategoryRepository();
-    const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+    const { createCategory } = makeSut();
 
-    const category = await createCategoryUseCase.execute({
+    const category = await createCategory.execute({
       name: 'any_name',
     });
 
@@ -15,17 +24,16 @@ describe('Create Category Use Case', () => {
   });
 
   it('should not be able to create an category with an used name', async () => {
-    const categoryRepository = new InMemoryCategoryRepository();
-    const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+    const { createCategory } = makeSut();
 
-    await createCategoryUseCase.execute({
+    await createCategory.execute({
       name: 'any_name',
     });
 
     expect(async () => {
-      await createCategoryUseCase.execute({
+      await createCategory.execute({
         name: 'any_name',
       });
-    }).rejects.toThrow(EntityAlreadyExistsError);
+    }).rejects.toThrow(CategoryAlreadyExistsException);
   });
 });
--- src/application/usecases/category-usecases/create-category-usecase.ts
+++ src/application/usecases/category-usecases/create-category-usecase.ts
@@ -1,6 +1,8 @@
 import { Category } from '@application/entities/category/category';
 import { CategoryRepository } from '@application/repositories/category-repository';
 import { Injectable } from '@nestjs/common';
+import { FindCategoryByNameUseCase } from './find-category-by-name-usecase';
+import { CategoryAlreadyExistsException } from '@infra/http/exceptions/category-already-exists-exception';
 
 export interface CreateCategoryRequest {
   name: string;
@@ -8,10 +10,18 @@ export interface CreateCategoryRequest {
 
 @Injectable()
 export class CreateCategoryUseCase {
-  constructor(private categoryRepository: CategoryRepository) {}
+  constructor(
+    private categoryRepository: CategoryRepository,
+    private findCategoryByName: FindCategoryByNameUseCase,
+  ) {}
 
   async execute(request: CreateCategoryRequest): Promise<Category> {
     const { name } = request;
+
+    const categoryExists = await this.findCategoryByName.execute(name);
+    if (categoryExists) {
+      throw new CategoryAlreadyExistsException();
+    }
 
     const category = new Category({
       name,
