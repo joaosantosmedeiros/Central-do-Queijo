a108c809e9aa3c4d39bea4f5814ba39af1428d6b
joaosantosmedeiros <jopesame@gmail.com>
2023-12-14 13:00:48 -0300

    fix: fix UpdateCartProductUseCase to throw if a negative amount is passed

--- src/application/usecases/cart-product-usecases/update-cart-product-usecase.spec.ts
+++ src/application/usecases/cart-product-usecases/update-cart-product-usecase.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCartProductRepository } from '@test/repositories/in-memory-cart-products-repository';
 import { UpdateCartProductUseCase } from './update-cart-product-usecase';
 import { CartProduct } from '@application/entities/cart-product/cart-product';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UpdateCartProductUseCase', () => {
   it('should update an cart product correctly', async () => {
@@ -21,6 +22,29 @@ describe('UpdateCartProductUseCase', () => {
       cartProductId: 'any_id',
     });
     expect(inMemoryCartProductRepository.cartProducts[0].amount).toBe(4);
+  });
+
+  it('should throw an error if a negative amount is passed', async () => {
+    const inMemoryCartProductRepository = new InMemoryCartProductRepository();
+    const updateCartProductUseCase = new UpdateCartProductUseCase(
+      inMemoryCartProductRepository,
+    );
+
+    inMemoryCartProductRepository.cartProducts.push(
+      new CartProduct(
+        { amount: 1, cartId: 'any_id', productId: 'any_id' },
+        'any_id',
+      ),
+    );
+
+    async () => {
+      expect(
+        await updateCartProductUseCase.execute({
+          amount: 0,
+          cartProductId: 'any_id',
+        }),
+      ).rejects.toThrow(BadRequestException);
+    };
   });
 
   it('should throw an error if an invalid product is passed', async () => {
--- src/application/usecases/cart-product-usecases/update-cart-product-usecase.ts
+++ src/application/usecases/cart-product-usecases/update-cart-product-usecase.ts
@@ -1,6 +1,6 @@
 import { CartProduct } from '@application/entities/cart-product/cart-product';
 import { CartProductRepository } from '@application/repositories/cart-product-repository';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 export interface UpdateCartProductRequest {
   amount: number;
@@ -12,6 +12,10 @@ export class UpdateCartProductUseCase {
   constructor(private cartProductRepository: CartProductRepository) {}
 
   async execute(request: UpdateCartProductRequest): Promise<CartProduct> {
+    if (request.amount < 1) {
+      throw new BadRequestException('Amount must be positive.');
+    }
+
     return this.cartProductRepository.update(
       request.amount,
       request.cartProductId,
