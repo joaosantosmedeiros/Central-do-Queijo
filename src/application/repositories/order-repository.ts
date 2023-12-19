60d158c5cc5bb426de7d1961b3f5b6caa8a1986c
joaosantosmedeiros <jopesame@gmail.com>
2023-12-19 14:19:36 -0300

    feat: add list method in orderRepository

--- src/application/repositories/order-repository.ts
+++ src/application/repositories/order-repository.ts
@@ -2,5 +2,6 @@ import { Order } from '@application/entities/order/order';
 
 export abstract class OrderRepository {
   abstract create(order: Order): Promise<Order>;
+  abstract list(): Promise<Order[]>;
   abstract findOrdersByAccountId(accountId: string): Promise<Order[]>;
 }
