0ef09e6a5bd73e254c0cae6710316de77c8649e4
joaosantosmedeiros <jopesame@gmail.com>
2023-12-21 13:46:51 -0300

    fix: fix OrderProduct entity product property

--- src/application/entities/order-product/order-product.ts
+++ src/application/entities/order-product/order-product.ts
@@ -27,6 +27,7 @@ export class OrderProduct {
       ...props,
       createdAt: props.createdAt ?? new Date(),
       updatedAt: props.updatedAt ?? new Date(),
+      product: props.product ? new Product(props.product) : undefined,
     };
   }
 
