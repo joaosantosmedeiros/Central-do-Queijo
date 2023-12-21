a524e0cbd14b1fb8271f8c486659d6fbe4e412a9
joaosantosmedeiros <jopesame@gmail.com>
2023-12-21 14:29:53 -0300

    feat: add findOrderById abstract method on OrderRepository

--- src/application/repositories/order-repository.ts
+++ src/application/repositories/order-repository.ts
@@ -3,5 +3,6 @@ import { Order } from '@application/entities/order/order';
 export abstract class OrderRepository {
   abstract create(order: Order): Promise<Order>;
   abstract list(): Promise<Order[]>;
+  abstract findOrderById(orderId: string): Promise<Order | null>;
   abstract findOrdersByAccountId(accountId: string): Promise<Order[]>;
 }
