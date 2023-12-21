2a8634654a4ce5dfe21ff82fc6cc6f60d225d2b6
joaosantosmedeiros <jopesame@gmail.com>
2023-12-21 14:52:03 -0300

    test: ensure FindOrderByIdUseCase works properly

---
+++ src/application/usecases/order-usecases/find-order-by-id-usecase.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryOrdersRepository } from '@test/repositories/in-memory-orders-repository';
+import { FindOrderByIdUseCase } from './find-order-by-id-usecase';
+import { Order } from '@application/entities/order/order';
+import { EntityNotFoundException } from '@infra/http/exceptions/entity-not-found-exception';
+
+describe('FindOrderByIdUseCase', () => {
+  it('should show an existing order correctly', async () => {
+    const orderRepository = new InMemoryOrdersRepository();
+    const findOrderById = new FindOrderByIdUseCase(orderRepository);
+
+    const order = new Order(
+      {
+        accountId: 'any_acc_id',
+        paymentId: 'any_pay_id',
+      },
+      'id-1',
+    );
+    orderRepository.orders.push(order);
+    const foundOrder = await findOrderById.execute('id-1');
+
+    expect(foundOrder).toEqual(order);
+  });
+
+  it('should throw if no order is found', async () => {
+    const orderRepository = new InMemoryOrdersRepository();
+    const findOrderById = new FindOrderByIdUseCase(orderRepository);
+
+    async () => {
+      expect(await findOrderById.execute('fake_id')).rejects.toThrow(
+        EntityNotFoundException,
+      );
+    };
+  });
+});
--- test/repositories/in-memory-orders-repository.ts
+++ test/repositories/in-memory-orders-repository.ts
@@ -4,6 +4,10 @@ import { OrderRepository } from '@application/repositories/order-repository';
 export class InMemoryOrdersRepository implements OrderRepository {
   public orders: Order[] = [];
 
+  async findOrderById(orderId: string): Promise<Order | null> {
+    return this.orders.find((order) => order.id === orderId) ?? null;
+  }
+
   async findOrdersByAccountId(accountId: string): Promise<Order[]> {
     return this.orders.filter((order) => order.accountId == accountId);
   }
