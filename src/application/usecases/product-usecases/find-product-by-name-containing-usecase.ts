2c7fac329893faa2d9463bceace8d4c38c11bba2
joaosantosmedeiros <jopesame@gmail.com>
2023-12-28 13:45:34 -0300

    feat: add FindProductByNameContainingUseCase

---
+++ src/application/usecases/product-usecases/find-product-by-name-containing-usecase.ts
@@ -0,0 +1,37 @@
+import { Product } from '@application/entities/product/product';
+import { ProductRepository } from '@application/repositories/product-repository';
+import {
+  PaginationDto,
+  PaginationMeta,
+} from '@infra/http/dto/others/pagination-dto';
+import { Injectable } from '@nestjs/common';
+
+const DEFAULT_PAGE_SIZE = 10;
+const FIRST_PAGE = 1;
+
+@Injectable()
+export class FindProductByNameContainingUseCase {
+  constructor(private productRepository: ProductRepository) {}
+
+  async execute(
+    search: string,
+    size = DEFAULT_PAGE_SIZE,
+    page = FIRST_PAGE,
+  ): Promise<PaginationDto<Product[]>> {
+    const [products, count] = await this.productRepository.findByNameContaining(
+      search,
+      size,
+      page,
+    );
+
+    return new PaginationDto(
+      new PaginationMeta(
+        Number(size),
+        count,
+        Number(page),
+        Math.ceil(count / size),
+      ),
+      products,
+    );
+  }
+}
